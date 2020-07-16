3869fa3ed9315d468eb046fa03f570b5df085854
apple <apple@apples-imac-6.attlocal.net>
2020-07-16 09:21:20 -0400

    multidimensional arrays

---
+++ src/day19arrays/Test01.java
@@ -0,0 +1,12 @@
+package day19arrays;
+
+import java.util.Arrays;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int arr[][] = new int[2][3];
+
+		System.out.println(Arrays.deepToString(arr));
+	}
+}
